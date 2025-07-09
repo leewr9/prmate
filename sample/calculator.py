01c84f993752cc8350ae377e13a52fa114b62eb3
Rooam Lee <leewr9@naver.com>
2025-07-09 15:50:47 +0900

    sample:: fix sample code

--- sample/calculator.py
+++ sample/calculator.py
@@ -9,21 +9,5 @@ def multiply(a, b):
 
 def divide(a, b):
     if b == 0:
-        return "Cannot divide by zero!"
+        raise ValueError("Cannot divide by zero!")
     return a / b
-
-
-# def add(a, b):
-#     return a + b
-# 
-# def subtract(a, b):
-#     return a - b
-# 
-# def multiply(a, b):
-#     return a * b
-# 
-# def divide(a, b):
-#     if b == 0:
-#         raise ValueError("Cannot divide by zero!")
-#     return a / b
-
