5b86b61a659e8c3d29a7fa64573588208895f099
Rooam Lee <leewr9@naver.com>
2025-07-11 15:46:33 +0900

    sample:: test pr review
    user

--- sample/user.py
+++ sample/user.py
@@ -1,23 +1,22 @@
 class User:
-    def __init__(self, username, email):
+    def __init__(self, username: str, email: str):
         self.username = username
         self.email = email
         self.logged_in = False
 
-    def login(self):
+    def login(self) -> None:
         self.logged_in = True
 
-    def logout(self):
+    def logout(self) -> None:
         self.logged_in = False
 
-    def is_logged_in(self):
+    def is_logged_in(self) -> bool:
         return self.logged_in
 
-    def update_email(self, new_email):
+    def update_email(self, new_email: str) -> None:
         if "@" not in new_email:
-            print("Invalid email address!")
-        else:
-            self.email = new_email
+            raise ValueError("Invalid email address")
+        self.email = new_email
 
 
 # class User:
